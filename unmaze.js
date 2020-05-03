f462af6c61e3ece3136c6f22485c0320434d7319
EGE <criticalstack@macitege.com>
2020-05-02 23:06:27 -0400

    progress

--- unmaze.js
+++ unmaze.js
@@ -6,78 +6,69 @@ function unmaze(obj) {
 }
 
 class Unmaze extends Helpers {
-  constructor(obj) {
+  constructor(object) {
     super()
-    this._obj = { ...obj }
-    this._path = null
-    this._key = null
-    this._value = null
-  }
-
-  get value() {
-    return this._value
-  }
-
-  get key() {
-    return this._key
-  }
-
-  get path() {
-    return this._path
+    var _internalObj = { ...object }
+    this.getInternalObj = () => ({ ..._internalObj })
+    this.setInternalObj = payload => { _internalObj = payload }
+    this.path = null
+    this.key = null
+    this.value = null
   }
 
   get obj() {
-    return this._obj
+    console.log('getting internal object')
+    return this.getInternalObj()
   }
+
+  // set obj(payload) {
+  //   console.log('setting it')
+  //   this.setInternalObj(payload)
+  // }
 
   get(prop) {
-    this._key = prop
-    this._value = this.getValueOf(prop, this.obj)
-    this._path = this.getPathTo(prop, this.obj)
+    this.key = prop
+    this.value = this.getValueOf(prop, this.obj)
+    this.path = this.getPathTo(prop, this.obj)
     return this
   }
 
-  of(prop) {
-    const parent = this.getValueOf(prop, this.obj)
-    this._value = parent[this.key]
-    this._path = [
-      ...this.getPathTo(prop, this.obj),
-      this._path[this._path.length - 1],
-    ]
-    return this
-  }
+  // of(prop) {
+  //   const parent = this.getValueOf(prop, this.obj)
+  //   this.value = parent[this.key]
+  //   this.path = [
+  //     ...this.getPathTo(prop, this.obj),
+  //     this.path[this.path.length - 1],
+  //   ]
+  //   return this
+  // }
 
-  in(prop) {
-    return this.of(prop)
-  }
+  // in(prop) {
+  //   return this.of(prop)
+  // }
 
-  set set(val) {
-    // const path = this.getPathTo(prop, this.obj)
-    // let ref = this.obj
-    // for (let i = 0, len = path.length; i < len; ++i) {
-    //   ref = ref[path[i]]
-    // }
-    // return ref
-    console.log('this.value', this.value)
-    console.log('val', val)
-    console.log('path', path)
-    // CREATE THE SETTER FOR SYNTAX: o.get('something').set = 'new value'
-    // for (let i; i < this._path.length; i++) {}
-    // this._obj[0][1][2][3][4][5] = value
-    this._value = val
-  }
+  // set set(val) {
+  //   // const path = this.getPathTo(prop, this.obj)
+  //   // let ref = this.obj
+  //   // for (let i = 0, len = path.length; i < len; ++i) {
+  //   //   ref = ref[path[i]]
+  //   // }
+  //   // return ref
+  //   console.log('this.value', this.value)
+  //   console.log('val', val)
+  //   console.log('path', path)
+  //   // CREATE THE SETTER FOR SYNTAX: o.get('something').set = 'new value'
+  //   // for (let i; i < this.path.length; i++) {}
+  //   // this.obj[0][1][2][3][4][5] = value
+  //   this.value = val
+  // }
 }
 
 const o = unmaze(poke)
-/*
 
-o.get("url").of("move_learn_method").set("ege.com");
-o.get("url") = "ege.com";
-o.version_group_details.move_learn_method.url = "ege.com";
-
-*/
-// console.log(o.get('slot').in("details").path)
-// console.log(o.get('slot').of('details').value)
-
-console.log(o.get('name').in('base_experience').path)
-console.log(o.get('name').in('move').value)
+console.log(o.obj)
+o.obj = {...o.obj, ability: 10}
+console.log(o.obj)
+// Want to achieve this:
+// o.get('ability') = 10
+// console.log(o.obj)
